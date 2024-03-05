56622460ea070dc9935d8e33e0eb6a796da4afee
Bobby Dawson <me@bobbydawson.com>
2024-03-05 00:53:40 -0500

    config changes

--- tests/tempTest.php
+++ tests/tempTest.php
@@ -2,11 +2,12 @@
 require_once "../src/DonorPerfectApi.php";
 require_once "../src/types/BaseType.php";
 require_once "../src/types/DonorUserDefinedFieldType.php";
+include_once "../config/config.private.php";
 
 use DonorPerfectApi\DonorPerfectApi;
 
 
-$dpApi = new DonorPerfectApi("sHmOaMBhaDF7ml5wnralegttGDrosCpwRZYWgaXDd5JkkYyKnQE8IEgkufFWX3BFLSVY2A%2bg1iQRPKZ3fhYqhs0EdjOfLKjIIyhfefm0p0M%3d");
+$dpApi = new DonorPerfectApi(API_TEST_KEY);
 
 $result = $dpApi->updateDonorUserDefinedFields(143, ['DS_RATING' => "DS1-2", "NO_GIFT_MATCHES" => 55, 'MATCH_QUALITY' => 19.9]);
 
